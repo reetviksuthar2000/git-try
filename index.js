98884dd53d48dbbe25e0f488f5ba54566eb7d46f
reetviksuthar2000 <reetviksuthar1@gmail.com>
2023-07-30 22:17:32 +0530

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get('/data', (req, res) => {
     res.send('data is and solve nodemon problem')
   })
   
+app.get('/data', (req, res) => {
+    res.send('data is and solve nodemon problem')
+  })
+  
+app.get('/data', (req, res) => {
+    res.send('data is and solve nodemon problem')
+  })
+  
 
 app.listen(4000, ()=>{
     console.log('server listening on port http://localhost:4000')
